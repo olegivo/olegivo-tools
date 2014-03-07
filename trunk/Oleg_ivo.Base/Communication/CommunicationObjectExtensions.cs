75ae16ffd99d9a12d4807b2a323e42d71e631143
olegivo@gmail.com <olegivo@gmail.com@2cffb96a-f77c-fccb-4319-01ee8060965a>
2014-03-07 04:35:22 +0000

    Добавлены расширения работы с ICommunicationObject: Безопасный вызов и закрытие

---
+++ trunk/Oleg_ivo.Base/Communication/CommunicationObjectExtensions.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace Oleg_ivo.Base.Communication
+{
+    /// <summary>
+    /// Расширения работы с <see cref="ICommunicationObject"/>
+    /// </summary>
+    public static class CommunicationObjectExtensions
+    {
+        /// <summary>
+        /// Безопасный вызов и закрытие <see cref="client"/>
+        /// </summary>
+        /// <typeparam name="TResult">Тип результата вызова метода клиента</typeparam>
+        /// <typeparam name="TService">Тип клиента (должен реализовать <see cref="ICommunicationObject"/>)</typeparam>
+        /// <param name="client">Инстанс клиента</param>
+        /// <param name="method">Метод, вызываемый для клиента</param>
+        /// <returns></returns>
+        public static TResult MakeSafeServiceCall<TResult, TService>(this TService client, Func<TService, TResult> method) where TService : ICommunicationObject
+        {
+            TResult result;
+
+            try
+            {
+                result = method(client);
+            }
+            finally
+            {
+                client.SafeClose();
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Безопасное закрытие <see cref="client"/>
+        /// </summary>
+        /// <typeparam name="TService">Тип клиента (должен реализовать <see cref="ICommunicationObject"/>)</typeparam>
+        /// <param name="client">Инстанс клиента</param>
+        public static void SafeClose<TService>(this TService client) where TService : ICommunicationObject
+        {
+            try
+            {
+                client.Close();
+            }
+            catch (CommunicationException)
+            {
+                client.Abort(); // Don't care about these exceptions. The call has completed anyway.
+            }
+            catch (TimeoutException)
+            {
+                client.Abort(); // Don't care about these exceptions. The call has completed anyway.
+            }
+            catch (Exception)
+            {
+                client.Abort();
+                throw;
+            }
+        }
+    }
+}
