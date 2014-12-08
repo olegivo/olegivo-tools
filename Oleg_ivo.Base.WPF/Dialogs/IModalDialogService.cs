c35e4d59b32c128fee1994dc759da51349062756
olegivo@gmail.com <olegivo@gmail.com>
2014-12-08 19:15:14 +0000

    Выделен проект для общих решений WPF В проект перенесена логика работы с диалоговыми окнами (заточено под MVVM)

---
+++ Oleg_ivo.Base.WPF/Dialogs/IModalDialogService.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Oleg_ivo.Base.WPF.Dialogs
+{
+    public interface IModalDialogService
+    {
+        /// <summary>
+        /// Показать диалог
+        /// </summary>
+        /// <typeparam name="TDialogViewModel">Тип модели представления</typeparam>
+        /// <param name="view">Представление</param>
+        /// <param name="onSetup">Делегат для настройки диалога</param>
+        /// <param name="onDialogClose">Делегат, срабатывающий после закрытия диалога</param>
+        void ShowDialog<TDialogViewModel>(IModalWindow<TDialogViewModel> view, Action<IModalWindow<TDialogViewModel>> onSetup, Action<TDialogViewModel, bool?> onDialogClose);
+
+        /*/// <summary>
+        /// Показать диалог
+        /// </summary>
+        /// <typeparam name="TDialogViewModel">Тип модели представления</typeparam>
+        /// <param name="view">Представление</param>
+        /// <param name="onDialogClose">Делегат, срабатывающий после закрытия диалога</param>
+        void ShowDialog<TDialogViewModel>(IModalWindow<TDialogViewModel> view, Action<TDialogViewModel, bool?> onDialogClose);*/
+
+        /// <summary>
+        /// Показать диалог
+        /// </summary>
+        /// <typeparam name="TDialogViewModel">Тип модели представления</typeparam>
+        /// <param name="view">Представление</param>
+        /// <param name="onSetup">Делегат для настройки диалога</param>
+        void ShowDialog<TDialogViewModel>(IModalWindow<TDialogViewModel> view, Action<IModalWindow<TDialogViewModel>> onSetup);
+
+        /// <summary>
+        /// Показать диалог
+        /// </summary>
+        /// <typeparam name="TDialogViewModel">Тип модели представления</typeparam>
+        /// <param name="view">Представление</param>
+        void ShowDialog<TDialogViewModel>(IModalWindow<TDialogViewModel> view);
+        
+        /// <summary>
+        /// Показать диалог (представление создаётся внутри)
+        /// </summary>
+        /// <typeparam name="TDialogViewModel">Тип модели представления</typeparam>
+        /// <param name="onSetup">Делегат для настройки диалога</param>
+        /// <param name="onDialogClose">Делегат, срабатывающий после закрытия диалога</param>
+        void CreateAndShowDialog<TDialogViewModel>(Action<IModalWindow<TDialogViewModel>> onSetup, Action<TDialogViewModel, bool?> onDialogClose);
+
+        /*/// <summary>
+        /// Показать диалог (представление создаётся внутри)
+        /// </summary>
+        /// <typeparam name="TDialogViewModel">Тип модели представления</typeparam>
+        /// <param name="onDialogClose">Делегат, срабатывающий после закрытия диалога</param>
+        void CreateAndShowDialog<TDialogViewModel>(Action<TDialogViewModel, bool?> onDialogClose);*/
+
+        /// <summary>
+        /// Показать диалог (представление создаётся внутри)
+        /// </summary>
+        /// <typeparam name="TDialogViewModel">Тип модели представления</typeparam>
+        /// <param name="onSetup">Делегат для настройки диалога</param>
+        void CreateAndShowDialog<TDialogViewModel>(Action<IModalWindow<TDialogViewModel>> onSetup);
+
+        /// <summary>
+        /// Показать диалог (представление создаётся внутри)
+        /// </summary>
+        /// <typeparam name="TDialogViewModel">Тип модели представления</typeparam>
+        void CreateAndShowDialog<TDialogViewModel>();
+        
+        /// <summary>
+        /// Получить новое диалоговое представление
+        /// </summary>
+        /// <typeparam name="TViewModel">Тип модели представления</typeparam>
+        /// <returns></returns>
+        IModalWindow<TViewModel> CreateDialog<TViewModel>();
+    }
+}
---
+++ Oleg_ivo.Base.WPF/Dialogs/IModalWindow.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Oleg_ivo.Base.WPF.Dialogs
+{
+    public interface IModalWindow<TViewModel>
+    {
+        bool? DialogResult { get; set; }
+        event EventHandler Closed;
+        void Show();
+        bool? ShowDialog();
+        TViewModel ViewModel { get; set; }
+        string Title { get; set; }
+        void Close();
+    }
+}
