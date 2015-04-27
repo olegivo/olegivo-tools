edc6d1b6f3877a1829456a50bcb920989372cacb
olegivo <olegivo@gmail.com>
2015-04-27 11:14:52 +0300

    Базовые представления: ContentControl- и Window-based

---
+++ Oleg_ivo.Base.WPF/ContentControlViewBase.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Controls;
+using Oleg_ivo.Base.Autofac.DependencyInjection;
+using Oleg_ivo.Base.WPF.ViewModels;
+
+namespace Oleg_ivo.Base.WPF
+{
+    public class ContentControlViewBase<TViewModel> : ContentControl where TViewModel : ViewModelBase
+    {
+
+        [Dependency(Required = true)]
+        public TViewModel ViewModel
+        {
+            get { return (TViewModel)DataContext; }
+            set { DataContext = value; }
+        }
+    }
+}
---
+++ Oleg_ivo.Base.WPF/WindowViewBase.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using Oleg_ivo.Base.Autofac.DependencyInjection;
+using Oleg_ivo.Base.WPF.ViewModels;
+
+namespace Oleg_ivo.Base.WPF
+{
+    public class WindowViewBase<TViewModel> : Window where TViewModel : ViewModelBase
+    {
+
+        [Dependency(Required = true)]
+        public TViewModel ViewModel
+        {
+            get { return (TViewModel)DataContext; }
+            set { DataContext = value; }
+        }
+    }
+}
