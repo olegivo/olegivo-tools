5c98a3dbefc3056740f42112798b7da31ed944bb
olegivo <olegivo@gmail.com>
2015-08-15 16:09:17 +0300

    Утилитарный класс для замера периода работы кода
    Реализация IDisposible позволяет оборачивать замеряемый код в using

---
+++ Oleg_ivo.Tools/Utils/Time.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Oleg_ivo.Tools.Utils
+{
+    public class ElapsedAction : IDisposable
+    {
+        private Action<TimeSpan> postAction;
+        private Stopwatch stopwatch;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:System.Object"/> class.
+        /// </summary>
+        public ElapsedAction(Action<TimeSpan> postAction)
+        {
+            this.postAction = postAction;
+            stopwatch = Stopwatch.StartNew();
+        }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            if (stopwatch == null) return;
+            stopwatch.Stop();
+            postAction(stopwatch.Elapsed);
+            stopwatch = null;
+            postAction = null;
+        }
+    }
+}
