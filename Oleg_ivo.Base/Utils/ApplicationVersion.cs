3d3a1964b4d1bc5502222e4f8a34f536f8e07a87
olegivo <olegivo@gmail.com>
2016-02-15 22:33:16 +0300

    ApplicationVersionHelper, IApplicationVersionProvider

---
+++ Oleg_ivo.Base/Utils/ApplicationVersion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reflection;
+
+namespace Oleg_ivo.Base.Utils
+{
+    public interface IApplicationVersionProvider
+    {
+        Version ApplicationVersion { get; }
+    }
+
+    public static class ApplicationVersionHelper
+    {
+        public static Version GetVersion(Type type)
+        {
+            return new Version(type.Assembly.GetCustomAttribute<AssemblyFileVersionAttribute>().Version);
+        }
+    }
+}
